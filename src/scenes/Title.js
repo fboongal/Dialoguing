9812055d85abea327c5549c76ad75fd29ddf1132
Franchesca Boongaling <fboongal@ucsc.edu>
2024-02-28 15:52:17 -0800

    first commit

---
+++ src/scenes/Title.js
@@ -0,0 +1,41 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super("titleScene")
+    }
+
+    preload() {
+        // load assets
+        this.load.path = "./assets/"
+
+        // load JSON (ie dialog text)
+        this.load.json('dialog', 'json/dialog.json')
+
+        // load images
+        this.load.image('dialogbox', 'img/dialogbox.png')
+        this.load.image('homer', 'img/homer.png')
+        this.load.image('minerva', 'img/minerva.png')
+        this.load.image('jove', 'img/jove.png')
+        this.load.image('neptune', 'img/neptune.png')
+        this.load.image('naruto', 'img/Naruto.png')
+
+        // load bitmap font
+        this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+        this.load.bitmapFont('EL_font', 'font/EL.png', 'font/EL.xml')
+    }
+
+    create() {
+        // add title text
+        this.add.bitmapText(centerX, centerY - 50, 'EL_font', 'THE ODYSSEY', 56).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'EL_font', 'Press [SPACE] to start', 42).setOrigin(0.5)
+
+        // create input
+        cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // wait for player input
+        if(Phaser.Input.Keyboard.JustDown(cursors.space)) {
+            this.scene.start("talkingScene")
+        }
+    }
+}
